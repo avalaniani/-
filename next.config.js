e4df7e88da1f16a7c259d4f92054f6df3ccfbfa6
avalaniani <luzonamit1999@gmail.com>
2026-03-01 19:17:57 +0200

    Add redirects to next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/app.html',
+        permanent: false,
+      },
+      {
+        source: '/app',
+        destination: '/app.html',
+        permanent: false,
+      },
+    ]
+  },
+}
 module.exports = nextConfig
